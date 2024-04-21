eef732264fbfb6ab1529af4fb2f7d0a0048110c2
Marcio Gabriel <gabriel09@gmail.com>
2024-04-21 01:52:49 -0300

    Update

--- questionnaires/src/main/java/com/hiro/questionnaires/controller/UserController.java
+++ questionnaires/src/main/java/com/hiro/questionnaires/controller/UserController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +14,6 @@ import com.hiro.questionnaires.dto.CreateUserDto;
 import com.hiro.questionnaires.dto.UserResponse;
 import com.hiro.questionnaires.service.UserService;
 
-@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 public class UserController {
     @Autowired
